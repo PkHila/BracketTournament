0fee3f2c767bc60b39af635aa6c99dcd7c4bfac8
NDondero <118403082+NDondero@users.noreply.github.com>
2023-06-19 10:12:32 -0300

    Update Resultado.java
    agregadas funcionalidades para que el resultado se muestre con el listado de perdedores en orden desde final hasta priemra ronda con indicacion de cual ronda

--- src/Modelo/Resultados/Resultado.java
+++ src/Modelo/Resultados/Resultado.java
@@ -4,9 +4,7 @@ import Modelo.Competidor;
 import Modelo.Enfrentamiento;
 
 import java.io.Serializable;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Resultado implements Serializable {
     private int cantidadDeRondas; // con esta informacion se puede inferir en que ronda perdio cada Competidor que no es el ganador
@@ -55,12 +53,35 @@ public class Resultado implements Serializable {
                 '}';
     }
     private String listarPerdedores() {
-        String listado = "";
         Iterator<Map.Entry<Competidor,Competidor>> it = perdedores.entrySet().iterator();
+        StringBuilder listado = new StringBuilder();
+        Stack<Map.Entry<Competidor,Competidor>> entries = new Stack<>();
+        int contador = 1;
         while (it.hasNext()) {
-            Map.Entry<Competidor,Competidor> entry = it.next();
-            listado += entry.getKey().getNombre() + " ante " + entry.getValue().getNombre() + " en ronda x" + "\n";
+            entries.push(it.next());
         }
-        return listado;
+        while (!entries.isEmpty()) {
+            Map.Entry<Competidor,Competidor> entry = entries.pop();
+            String ronda = "";
+            if(contador == 1) {
+                ronda = "final";
+            }
+            listado.append(entry.getKey().getNombre() + " ante " + entry.getValue().getNombre() + " en " + queRondaEs(contador) + "\n");
+            contador++;
+        }
+        return listado.toString();
+    }
+    private String queRondaEs(int contador) {
+        String ronda = "";
+        if(contador == 1) {
+            ronda = "ronda final";
+        } else if (contador < 4 && this.cantidadDeRondas > 2) {
+            ronda = "ronda semi-final";
+        } else if (contador < 8 && this.cantidadDeRondas > 3) {
+            ronda = "cuartos de final";
+        } else {
+            ronda = "primera ronda";
+        }
+        return ronda;
     }
 }
