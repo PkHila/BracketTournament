da400d9b511932cd3ea7a8187b0cf519080f97e6
PkHila <105527173+PkHila@users.noreply.github.com>
2023-06-23 09:11:02 -0300

    nueva IndexOutOfBoundsException para listarPlantillas

--- src/Modelo/Menu/Menu.java
+++ src/Modelo/Menu/Menu.java
@@ -252,23 +252,22 @@ public class Menu {
         }
 
         for (int i = 0; i < listaPlantillas.size(); i++) {
-            System.out.println(i+1 + ": " + listaPlantillas.get(i) + "\n");
+            System.out.println((i+1) + ": " + listaPlantillas.get(i));
         }
         if(listaPlantillas.size() != 0) {
             System.out.println("elegir plantilla:");
             eleccion = scanner.nextInt();
-            String nombre = listaPlantillas.get(eleccion-1);
-            plantilla = sistema.buscarPlantilla(nombre);
+            if(eleccion > 0 && eleccion <= listaPlantillas.size()){
+                String nombre = listaPlantillas.get(eleccion-1);
+                plantilla = sistema.buscarPlantilla(nombre);
+            }
+            else {
+                throw new IndexOutOfBoundsException();
+            }
+
         }
         else {
             System.out.println("No hay plantillas cargadas");
-            /*System.out.println("No hay plantillas cargadas. Queres crear una?\n1. Sí\n2. No");
-            eleccion = scanner.nextInt();
-            if(eleccion == 1) {
-                plantilla = crearNuevoTorneo();
-            }*/
-            //todo ver si sacar esto
-            
         }
         return plantilla;
     }
